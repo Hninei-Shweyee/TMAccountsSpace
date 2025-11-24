f2f787849d845724fdee1711ac4ef13dc90315e0
Hnin Ei Shwe Yee <emerald@Hnins-MacBook-Air-10.local>
2025-11-25 03:14:46 +0700

    Move src out of public and fix structure

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+// import router from './router'
+// import './firebase' // if you have initialization here
+
+// new Vue({
+//   router,
+//   render: h => h(App),
+// }).$mount('#app')
+
